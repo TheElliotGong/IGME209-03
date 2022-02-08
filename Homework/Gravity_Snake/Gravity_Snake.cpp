d595b9d3922555ac6165906861ab9607c23d1789
thetyper101 <ewcgong@hotmail.com>
2022-02-08 13:15:53 -0500

    created code to create the snake player, represented by a dynamic body

--- Homework/Gravity_Snake/Gravity_Snake.cpp
+++ Homework/Gravity_Snake/Gravity_Snake.cpp
@@ -10,12 +10,19 @@ int main()
 	b2Vec2 gravity(0.0f, -9.8f);
 	b2World world(gravity);
 
+	/*
 	b2BodyDef boxDef;
 	boxDef.position.SetZero();
 
 	b2Body* box = world.CreateBody(&boxDef);
 	b2PolygonShape area;
-	area.SetAsBox(2.5f, 2.5f);
+	area.SetAsBox(2.5f, 2.5f);*/
+	
+	//Create the dynamic body that represents the snake player
+	b2BodyDef snake;
+	snake.type = b2_dynamicBody;
+	snake.position.Set(0.0f, 0.0f);
+	b2Body* player = world.CreateBody(&snake);
 	getch();
 
 }
