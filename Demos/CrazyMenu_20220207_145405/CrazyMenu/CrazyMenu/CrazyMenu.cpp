937db481d65dcc4cd50b300341e30b6c3e14da35
thetyper101 <ewcgong@hotmail.com>
2022-02-07 21:40:23 -0500

    added comments to better explain the code.

--- Demos/CrazyMenu_20220207_145405/CrazyMenu/CrazyMenu/CrazyMenu.cpp
+++ Demos/CrazyMenu_20220207_145405/CrazyMenu/CrazyMenu/CrazyMenu.cpp
@@ -23,19 +23,23 @@ void Answer()
 }
 
 
-
+//Define new data types of pointers that point to void functions. One has no parameters, while the
+//other takes an int param.
 typedef void (*ActionPtr)();
 typedef void (*ProgressPtr)(int percentage);
 
+//Declare a void function that takes an int param.
 void PrintProgress(int percent)
 {
     cout << "progress is " << percent << endl;
 }
 
+//Declare a function that calls one of the custom data types.
 void ShowProgress(ProgressPtr callback)
 {
     for (int i = 0; i <= 100; i+=10)
     {
+        //If pointer function isn't null, call it using the int parameter.
         if (callback != nullptr)
             (*callback)(i);
     }
@@ -43,6 +47,7 @@ void ShowProgress(ProgressPtr callback)
 
 int main()
 {
+
     typedef char PlayerAttribute;
     PlayerAttribute str;
     PlayerAttribute wis;
@@ -53,20 +58,21 @@ int main()
     {
         PrintMenu();
         cin >> input;
+        //Define a custom pointer type that points to a void function.
         ActionPtr currentAction = nullptr;
-
+        //Have the pointer point to the specific function based on input.
         if (input == '1')
             currentAction = &SayHello;
         else if (input == '2')
             currentAction = &Answer;
 
-
+        //Call the function via the pointer.
         if (currentAction != nullptr)
             (*currentAction)();
 
     } while (input != 'Q');
 
-
+    //use the address of a function as a field for the custom pointer function.
     ShowProgress(&PrintProgress);
 
 }
