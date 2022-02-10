978722deea1242e58fa073ea96d19b3a62cb2500
thetyper101 <ewcgong@hotmail.com>
2022-02-09 23:13:32 -0500

    Added some more statements to pointer demo

--- Demos/MyPointerDemo/MyPointerDemo.cpp
+++ Demos/MyPointerDemo/MyPointerDemo.cpp
@@ -12,10 +12,14 @@ int main()
     Piece pieces[maxPieces] = {};
     Piece* loop = nullptr;
 
-    for (int i = 0; i < maxPieces; i++)
+    for (i = 0, loop = pieces; i < maxPieces && loop != nullptr; i++, loop++)
     {
+        //pieces[i].weight = (i + 1 ) * 1000;
+       // strcpy_s(loop->Name, (char*)('A' + i));
+        loop->weight = (i + 1) * 1000; //This is faster than if we normally loop through an array.
 
-    }
+        loop[i].weight = (i + 1) * 1000; //Does the same thing as above, just slower.
+    }   
 
     std::cout << "Hello World!\n";
 }
