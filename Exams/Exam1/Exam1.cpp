b7824133a397d70bce9bbfa5a639a7edbbddcd99
thetyper101 <ewcgong@hotmail.com>
2022-02-21 18:24:08 -0500

    fixed the case of empty input

--- Exams/Exam1/Exam1.cpp
+++ Exams/Exam1/Exam1.cpp
@@ -11,6 +11,7 @@
 #include <cctype>
 #include <math.h>
 #include <stdio.h>
+#include <stdlib.h>
 using namespace std;
 
 
@@ -18,49 +19,47 @@ using namespace std;
 void AverageScore(float* scores, int count);
 void HighScore(float* scores, int count);
 void LowScore(float* scores, int count);
+void GetScoreCount(char scoreCountInput[], int& intVar);
+void GetCurrentScore(char scoreInput[], float& floatVar);
 //float CharArrayToFloat(char* array);
 
 int main()
 {
     //This int variable keeps track of how many scores there are.
     int numOfScores;
+    float currentScore;
+    //This array keeps track of the string input used to determine the # of scores.
+    char input[30];
+    //This array keeps track of the string input used to enter float scores.
+    char scoreInput[50];
     //Ask the user for how many scores they want to use.
-    cout << "Enter the number of scores to use. No strings or numbers less than or equal to 0 allowed: ";
-    cin >> numOfScores;
-    //Keep asking them for input if the input is invalid or if the inputted number is less than or 
-    //equal to 0.
-    while (!cin || numOfScores <= 0)
+    //Keep asking them for input if the input is invalid, empty, or if the inputted number 
+    //is less than or equal to 0.
+    GetScoreCount(input, numOfScores);
+    while(numOfScores <= 0 || strlen(input) == 0)
     {
         //Clear input stream for the next sequence.
-        cin.clear();
-        cin.ignore(256, '\n');
         //Reprompt the user for input.
-        cout << "Enter the number of scores to use. No strings or numbers less than or equal to 0 allowed: ";
-        cin >> numOfScores;
+        GetScoreCount(input, numOfScores);
     }
     //Create a pointer that points to # of inputted float values.
     float* scores = new float[numOfScores];
     //Ask for input to add values to pointer array.
     for (int i = 0; i < numOfScores; i++)
     {
-        //Clear any potential invalid input left over from the previous input sequence.
-        //If this is the first score to be entered, than any leftover data from inputting
-        //the # of scores to be read will thankfully be cleared away.
-        cin.clear();
-        cin.ignore(256, '\n');
-        //Ask users for a score.
-        cout << "Enter a score: ";
-        cin >> scores[i];
-        //Keep asking for a score if the input is invalid.
-        while (!cin || scores[i] < 0 )
+        //Prompt user input, and parse 
+        cout << "No strings, blank input, or numbers less or equal to than 0 allowed. Enter a Score: ";
+        cin.getline(scoreInput, 50);
+        currentScore = atof(scoreInput);
+        while (currentScore <= 0 || strlen(scoreInput) == 0)
         {
-            //Clear the input again to make room for the next sequence.
-            cin.clear();
-            cin.ignore(256, '\n');
             //Reprompt them for input.
-            cout << "Input cannot be a string. Try again: ";
-            cin >> scores[i];
+            cout << "No strings, blank input, or numbers less or equal to than 0 allowed. Enter a Score: ";
+            cin.getline(scoreInput, 50);
+            currentScore = atof(scoreInput);
         }
+        //After input is finally valid, add it to the pointer array.
+        scores[i] = currentScore;
     }
     //Call all 3 functions.
     AverageScore(scores, numOfScores);
@@ -131,4 +130,22 @@ void LowScore(float* scores, int count)
     //Print out the final lowest score.
     cout << "Low Score is " << lowestScore << "\n";
 }
+/// <summary>
+/// This function takes a char array and parses any possible int value from it, and assign
+/// that value to the reference int parameter.
+/// </summary>
+/// <param name="scoreCountInput">The input/char array to parse the desired score count from.</param>
+/// <param name="value">The variable to assign any int value to.</param>
+void GetScoreCount(char scoreCountInput[], int& intVar)
+{
+    //Prompt the user for input and save it to the array.
+    cout << "Enter the number of scores to use. No strings, empty input, or numbers less than or equal to 0 allowed: ";
+    cin.getline(scoreCountInput, 70);
+    //Whatever int value was parsed from the int, save it to the reference parameter.
+    intVar = atoi(scoreCountInput);
+}
 
+void GetCurrentScore(char scoreInput[], float& floatVar)
+{
+
+}
